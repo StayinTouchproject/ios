6288b608c5c0d7735830d79c5c3556e21f774811
malnakli <hassoun@outlook.com>
2014-06-11 19:58:20 -0400

    and share extension

---
+++ IOS_stay_in_touch/share_with_grandma/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  share_with_grandma
+//
+//  Created by MoAir on 2014-06-11.
+//  Copyright (c) 2014 Stay_in_Touch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
---
+++ IOS_stay_in_touch/share_with_grandma/ShareViewController.m
@@ -0,0 +1,35 @@
+//
+//  ShareViewController.m
+//  share_with_grandma
+//
+//  Created by MoAir on 2014-06-11.
+//  Copyright (c) 2014 Stay_in_Touch. All rights reserved.
+//
+
+#import "ShareViewController.h"
+#import <Social/Social.h>
+
+@interface ShareViewController ()
+
+@end
+
+@implementation ShareViewController
+
+- (BOOL)isContentValid {
+    // Do validation of contentText and/or NSExtensionContext attachments here
+    return YES;
+}
+
+- (void)didSelectPost {
+    // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+    // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+    [self.extensionContext completeRequestReturningItems:nil completionHandler:nil];
+}
+
+- (NSArray *)configurationItems {
+    // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+    return @[];
+}
+
+@end
